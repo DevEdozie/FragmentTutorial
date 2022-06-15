d1a6dbc875b51af5ea0cc6b0cd5b50a599789b88
nicholaus michael <nicholausmichael51@gmail.com>
2022-06-15 23:23:41 +0100

    first commit

---
+++ app/src/main/java/com/creeds_code/fragmenttutorial/ToolbarFragment.java
@@ -0,0 +1,30 @@
+package com.creeds_code.fragmenttutorial;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.creeds_code.fragmenttutorial.databinding.FragmentToolbarBinding;
+
+public class ToolbarFragment extends Fragment {
+
+    FragmentToolbarBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentToolbarBinding.inflate(inflater,container,false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
